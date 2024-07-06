d63267cb5e0c42c33d0d4e0509d36691e0da3190
yvettegahamanyi <ygahamanyi26@gmail.com>
2024-07-06 13:09:43 +0200

    update 0-sub

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,21 +1,19 @@
 #!/usr/bin/python3
-"""
-function that queries the 'Reddit API' and returns the number of subscribers
-"""
+"""DOCUMENTING"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """
-    number of subscribers
-    """
-    url = "https://oauth.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0"}  # avoid Too Many Requests error
+    """DOC"""
+    reddit_url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
 
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    header = {'User-agent': 'Mozilla/5.0'}
+    response = requests.get(reddit_url,
+                            headers=header
+                            )
 
     if response.status_code == 200:
         data = response.json()
-        return data['data']['subscribers']
-    else:
-        return 0
+        subs = data['data']['subscribers']
+        return subs
+    return 0
